efe39a89941ccace2d9240580c6ce0ea4d78fcba
PedroMM64 <snupis3@live.com.mx>
2024-01-28 18:37:19 -0600

    Se agregan estados para los precios y los tipos de servicio

--- src/componentes/FormSubs.jsx
+++ src/componentes/FormSubs.jsx
@@ -1,11 +1,33 @@
-const FornSubs = () => {
+import { useState } from "react";
+
+const FormSubs = ({ setType, setPrice, type, price, setSubs, subs }) => {
+    const [error, setError] = useState(false);
+
+    const handleSubs = e =>{
+        e.preventDefault();
+        if (price === "" || Number(price) < 0 || type === "") {
+            setError(true);
+            return
+        }
+        setError(false);
+        
+        const data = {
+            type: type,
+            price: price,
+            id: Date.now()
+        }
+
+        setSubs([...subs, data])
+        
+    }
+
     return ( 
         <div className="add-subscription">
             <h3>Agregar Subscripciones</h3>
-            <form action="">
+            <form onSubmit={ handleSubs }>
                 <p>Servicio</p>
-                <select name="" id="">
-                    <option value="">--Elegir una subscripcion</option>
+                <select onChange={e => setType(e.target.value) }>
+                    <option value="">--Elegir una subscripción--</option>
                     <option value="netflix">Netflix</option>
                     <option value="disneyPlus">Disney Plus</option>
                     <option value="hboMax">HBO Max</option>
@@ -15,11 +37,12 @@ const FornSubs = () => {
                     <option value="appleTv">Apple TV</option>
                 </select>
                 <p>Cantidad</p>
-                <input type="number" placeholder="$20" />
+                <input type="number" placeholder="$20" onChange={e => setPrice(e.target.value) } />
                 <input type="submit" value="Agregar" />
             </form>
+            {error ? <p className="error">Campos inválidos</p> : null}
         </div>
     );
 }
 
-export default FornSubs;
+export default FormSubs;
--- src/componentes/MainControl.jsx
+++ src/componentes/MainControl.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import Balance from "./Balance";
-import FornSubs from "./FormSubs";
+import FormSubs from "./FormSubs";
 
 const MainControl = ({ count }) => {
+    const [subs, setSubs] = useState([])
+    const [type, setType] = useState("");
+    const [price, setPrice] = useState("");
+
     return (
         <div className="main-form">
             <Balance count = {count} />
-            <FornSubs />
+            <FormSubs 
+                setType = {setType} 
+                setPrice = {setPrice} 
+                type = {type} 
+                price = {price} 
+                setSubs = {setSubs} 
+                subs = {subs}
+                />
         </div>
     );
 }
